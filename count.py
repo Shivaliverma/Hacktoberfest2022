6411727c82553d38da518bdd1b23212508f632f8
Shivaliverma <48949691+Shivaliverma@users.noreply.github.com>
2022-10-29 19:44:20 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+lst = ['Cat', 'Bat', 'Sat', 'Cat', 'Mat', 'Cat', 'Sat']
+ 
+# To get the number of occurrences
+# of each item in a list
+print ([ [l, lst.count(l)] for l in set(lst)])
+ 
+# To get the number of occurrences
+# of each item in a dictionary
+print (dict( (l, lst.count(l) ) for l in set(lst)))
